aaefc95b84dd873207d75127d1297542dbcaf0cb
Asyraf Hussin <asyrafhussin4@gmail.com>
2022-07-01 22:03:14 +0800

    Fix google analytics

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,12 @@
-import * as gtag from "../lib/gtag";
-
 import Card from "../components/Card";
 import Footer from "../components/Footer";
 import Meta from "../components/Meta";
 import Navbar from "../components/Navbar";
-import Script from "next/script";
 
 export default function Home() {
   return (
     <>
       <Meta />
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-MT3GSW79HP"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-MT3GSW79HP');
-        `}
-      </Script>
 
       <Navbar />
       <Card />
