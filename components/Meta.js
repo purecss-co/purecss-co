03d04d17af73401631b13a61e59b36141a54bd78
Asyraf Hussin <asyrafhussin4@gmail.com>
2022-07-09 19:16:12 +0800

    Update metatag

--- components/Meta.js
+++ components/Meta.js
@@ -7,42 +7,30 @@ const defaults = {
   url: `https://purecss.co`,
 };
 
-export default function Meta(props) {
+export default function Meta() {
   return (
     <Head>
-      <title>{props.title || defaults.title}</title>
-      <meta
-        name="description"
-        content={props.description || defaults.description}
-      />
+      <title>{defaults.title}</title>
+      <meta name="description" content={defaults.description} />
 
       {/*<!-- Google / Search Engine Tags -->*/}
-      <meta itemProp="name" content={props.title || defaults.title} />
-      <meta
-        itemProp="description"
-        content={props.description || defaults.description}
-      />
-      <meta itemProp="image" content={props.image || defaults.image} />
+      <meta itemProp="name" content={defaults.title} />
+      <meta itemProp="description" content={defaults.description} />
+      <meta itemProp="image" content={defaults.image} />
 
       {/*<!-- Facebook Meta Tags -->*/}
-      <meta property="og:title" content={props.title || defaults.title} />
-      <meta
-        property="og:description"
-        content={props.description || defaults.description}
-      />
-      <meta property="og:image" content={props.image || defaults.image} />
-      <meta property="og:url" content={props.title || defaults.title} />
+      <meta property="og:title" content={defaults.title} />
+      <meta property="og:description" content={defaults.description} />
+      <meta property="og:image" content={defaults.image} />
+      <meta property="og:url" content={defaults.title} />
       <meta property="og:type" content="website" />
 
       {/*<!-- Twitter Meta Tags -->*/}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content="@asyrafhussin4" />
-      <meta name="twitter:title" content={props.title || defaults.title} />
-      <meta
-        name="twitter:description"
-        content={props.description || defaults.description}
-      />
-      <meta name="twitter:image" content={props.image || defaults.image} />
+      <meta name="twitter:title" content={defaults.title} />
+      <meta name="twitter:description" content={defaults.description} />
+      <meta name="twitter:image" content={defaults.image} />
       <meta name="twitter:image:alt" content="PureCSS Banner" />
     </Head>
   );
