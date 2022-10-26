0070f9fe00e909d51f9c6df47e92b8f8654a4874
Asyraf Hussin <asyrafhussin4@gmail.com>
2022-10-26 10:49:50 +0800

    Sperate twitter white icon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,6 +6,7 @@ import {
   YOUTUBE_URL,
 } from "../lib/constants";
 import { TwitterIcon, YoutubeIcon, GitHubIcon, FacebookIcon } from "./icons";
+import TwitterWhiteIcon from "./icons/TwitterWhiteIcon";
 
 const Navbar: NextPage = () => {
   return (
@@ -21,21 +22,7 @@ const Navbar: NextPage = () => {
             className="navbar-title-username"
             rel="noreferrer"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="icon-twitter"
-              width="12"
-              height="12"
-              viewBox="0 0 24 24"
-              strokeWidth="1"
-              stroke="#ffffff"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z" />
-            </svg>
+            <TwitterWhiteIcon />
             asyrafhussin4
           </a>
         </sup>
