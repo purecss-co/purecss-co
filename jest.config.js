8d23e6de8bc5be0592fd461d06da538c16680b38
Asyraf Hussin <asyrafhussin4@gmail.com>
2022-11-09 14:20:29 +0800

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -10,8 +10,10 @@ const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
-    "^@/components/(.*)$": "<rootDir>/components/$1",
     "^@/pages/(.*)$": "<rootDir>/pages/$1",
+    "^@/components/(.*)$": "<rootDir>/components/$1",
+    "^@/lib/(.*)$": "<rootDir>/lib/$1",
+    "^@/styles/(.*)$": "<rootDir>/styles/$1",
   },
   testEnvironment: "jest-environment-jsdom",
 };
