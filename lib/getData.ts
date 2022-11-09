06459ce364e43ecb7cba738bc15b18c86d901278
Asyraf Hussin <asyrafhussin4@gmail.com>
2022-11-09 14:04:23 +0800

    Fix fetchData

--- lib/getData.ts
+++ lib/getData.ts
@@ -3,7 +3,7 @@ import { APP_URL } from "./constants";
 
 export async function fetchData(): Promise<PureCSSData> {
   const res = await fetch(APP_URL + "api");
-  const data = await res.json();
+  const data = res.json();
 
   return data;
 }
