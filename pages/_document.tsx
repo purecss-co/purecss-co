600b2ce7939ca224a1f275be0f462527273d091b
Asyraf Hussin <asyrafhussin4@gmail.com>
2022-11-18 15:29:04 +0800

    Add lang tag to html

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ import { GA_TRACKING_ID } from "../lib/gtag";
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <Meta />
 
