72c0c3037f5603d276a136e6612ab6d0c061b757
Asyraf Hussin <asyrafhussin4@gmail.com>
2023-02-17 16:16:07 +0800

    Fix document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,11 +12,7 @@ export default class MyDocument extends Document {
           <Meta />
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;500;700&display=swap"
             rel="stylesheet"
