ba8cb8860e049f7ff594d8978fb9c1808e86aa58
Asyraf Hussin <asyrafhussin4@gmail.com>
2023-04-28 21:46:22 +0800

    Fix api

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,14 +1,15 @@
-import PureCSSData from "types/data";
 import { NextApiRequest, NextApiResponse } from "next";
-import path from "path";
+
+import PureCSSData from "types/data";
 import { promises as fs } from "fs";
+import path from "path";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<PureCSSData>,
 ) {
   const jsonDirectory = path.join(process.cwd(), "json");
-  const fileContents = await fs.readFile(jsonDirectory + "/data.json", "utf8");
+  const fileContents = await fs.readFile(`${jsonDirectory}/data.json`, "utf8");
 
   res.status(200).json(JSON.parse(fileContents));
 }
